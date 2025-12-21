f230b3483847c35f5254907aef06842facc6bf02
robostation777-eng <robostation777@gmail.com>
2025-12-22 05:21:38 +0800

    Modify CORS comment and update API endpoint
    Updated CORS comment and changed API endpoint version.

--- api/swap.js
+++ api/swap.js
@@ -1,10 +1,9 @@
 export default async function handler(req, res) {
-  // 加 CORS headers（支持跨域）
+  // CORS 支持跨域
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // 处理预检请求
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
@@ -43,7 +42,8 @@ export default async function handler(req, res) {
     return;
   }
 
-  const url = new URL('https://api.1inch.io/v6.0/42161/swap');
+  // 2025 年 12 月稳定 endpoint: v5.2 + .io 域名
+  const url = new URL('https://api.1inch.io/v5.2/42161/swap');
   Object.keys(queryParams).forEach(key => {
     if (queryParams[key]) {
       url.searchParams.append(key, queryParams[key]);
