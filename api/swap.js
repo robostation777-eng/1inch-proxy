6e5f53259064c8e576bfe02a663697b8250f5aa2
robostation777-eng <robostation777@gmail.com>
2025-12-22 05:07:15 +0800

    Update API endpoint to latest version

--- api/swap.js
+++ api/swap.js
@@ -32,8 +32,8 @@ export default async function handler(req, res) {
     return;
   }
 
-  // 正确官方 endpoint（无 /classic/）
-  const url = new URL('https://api.1inch.io/v6.0/42161/swap');
+  // 2025 年 12 月最新官方 endpoint：加 /swap 和 v6.1
+  const url = new URL('https://api.1inch.dev/swap/v6.1/42161/swap');
   Object.keys(queryParams).forEach(key => {
     if (queryParams[key]) {
       url.searchParams.append(key, queryParams[key]);
