ce3e1918a372259c7d542ac66f0655ae9988e913
robostation777-eng <robostation777@gmail.com>
2025-12-22 05:16:31 +0800

    Add CORS support and update API endpoint

--- api/swap.js
+++ api/swap.js
@@ -1,4 +1,15 @@
 export default async function handler(req, res) {
+  // 加 CORS headers（支持跨域）
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // 处理预检请求
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== 'GET') {
     res.status(405).end();
     return;
@@ -32,8 +43,7 @@ export default async function handler(req, res) {
     return;
   }
 
-  // 2025 年 12 月最新官方 endpoint：加 /swap 和 v6.1
-  const url = new URL('https://api.1inch.dev/swap/v6.1/42161/swap');
+  const url = new URL('https://api.1inch.io/v6.0/42161/swap');
   Object.keys(queryParams).forEach(key => {
     if (queryParams[key]) {
       url.searchParams.append(key, queryParams[key]);
